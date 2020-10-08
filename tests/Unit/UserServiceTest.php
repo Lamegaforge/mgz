9dc76a55c1d59ff13cab584c79dae8f6a6278ea8
Mathieu <degracia.mathieu@gmail.com>
2020-10-08 09:33:03 +0200

    Ajout d'un premier test

---
+++ tests/Unit/UserServiceTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Services\UserService;
+use Illuminate\Foundation\Testing\DatabaseMigrations; 
+
+class UserServiceTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     */
+    public function create_user()
+    {
+        $trackingId = 1;
+
+        $attributes = [
+            'tracking_id' => $trackingId,
+            'display_name' => 'Alan Grant',
+            'profile_image_url' => 'url',
+            'login' => 'alan_grant',
+        ];
+
+        $user = app(UserService::class)->firstOrCreate($trackingId, $attributes);
+
+        $this->assertInstanceOf(User::class, $user);
+
+        $this->assertEquals($user->id, $attributes['tracking_id']);
+        $this->assertEquals($user->display_name, $attributes['display_name']);
+        $this->assertEquals($user->profile_image_url, $attributes['profile_image_url']);
+        $this->assertEquals($user->login, $attributes['login']);
+    }
+}
