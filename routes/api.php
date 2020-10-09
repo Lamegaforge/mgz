5040fe6aef4119151725eb4d2dd9c00cdea347f3
Mathieu <degracia.mathieu@gmail.com>
2020-10-09 20:24:28 +0200

    Ajout du Api ClipController

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('clips')->as('clips.')->group(function () {
+	Route::get('search', 'ClipController@search')->name('search');
 });
