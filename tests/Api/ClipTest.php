f89ee318e20bd664abbd77782521fcfd5065e2d7
Mathieu <degracia.mathieu@gmail.com>
2020-10-11 12:12:58 +0200

    Amélioration des tests api clips

--- tests/Api/ClipTest.php
+++ tests/Api/ClipTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Api;
 
 use Tests\TestCase;
 use App\Models\Clip;
+use Illuminate\Support\Str;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ClipTest extends TestCase
@@ -25,5 +26,61 @@ class ClipTest extends TestCase
             ->assertStatus(200)
             ->assertJsonPath('clips.data.0.id', $clip->id)
             ->assertJsonPath('clips.data.0.card.id', $clip->card->id);
+    }
+
+    /**
+     * @test
+     */
+    public function get_clips_with_card_search()
+    {
+        $clip = Clip::factory()->create();
+
+        $clip->load('card');
+
+        $response = $this->get('api/clips/search', [
+            'card_id' => $clip->card->id,
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonPath('clips.data.0.id', $clip->id)
+            ->assertJsonPath('clips.data.0.card.id', $clip->card->id);
+    }
+
+    /**
+     * @test
+     */
+    public function get_clips_with_title_search()
+    {
+        $clip = Clip::factory()->create();
+
+        $clip->load('card');
+
+        $truncatedTitle = Str::limit($clip->title, 5, $end = null);
+
+        $response = $this->get('api/clips/search', [
+            'title' => $truncatedTitle,
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonPath('clips.data.0.id', $clip->id)
+            ->assertJsonPath('clips.data.0.card.id', $clip->card->id);
+    }
+
+    /**
+     * @test
+     */
+    public function cannot_get_inactive_clips()
+    {
+        $clip = Clip::factory()->create([
+            'active' => false,
+        ]);
+
+        $response = $this->get('api/clips/search');
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonMissing(['clips.data.0.id']);
     }
 }
