d319b6b202df93bc295606e1cdccb6dff13997f6
Mathieu <degracia.mathieu@gmail.com>
2020-10-12 19:14:18 +0200

    Ajout d'une directive datetime

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Carbon::setLocale('fr_FR');
+
+        Blade::directive('datetime', function ($carbon) {
+            return "<?php echo ($carbon)->isoFormat('LL'); ?>";
+        });
     }
 }
