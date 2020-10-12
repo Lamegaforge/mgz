ed1e6797cd12930db6b345491afaae1e68e3e8cb
Mathieu <degracia.mathieu@gmail.com>
2020-10-12 08:33:20 +0200

    Ajout d'un champ game dans le clip

--- database/factories/ClipFactory.php
+++ database/factories/ClipFactory.php
@@ -23,17 +23,32 @@ class ClipFactory extends Factory
      */
     public function definition()
     {
+        $games = $this->getGames();
+
         return [
             'tracking_id' => $this->faker->unique()->numberBetween(1000, 100000),
             'user_id' => User::factory(),
             'card_id' => Card::factory(),
             'slug' => $this->faker->slug,
             'title' => $this->faker->unique()->sentence($nbWords = 6, $variableNbWords = true),
+            'game' => $this->faker->randomElement($games),
             'thumbnail' => $this->faker->imageUrl($width = 640, $height = 480, 'cats'),
             'url' => $this->faker->unique()->url,
             'active' => true,
             'views' => $this->faker->numberBetween($min = 100, $max = 500),
             'approved_at' => $this->faker->dateTimeInInterval($startDate = '-30 years', $interval = '+ 5 days'),
+        ];
+    }
+
+    protected function getGames(): array
+    {
+        return [
+            'S.T.A.L.K.E.R.',
+            'Horizon Zero Dawn',
+            'Dino Crisis 2',
+            'DOOM',
+            'River City Girls',
+            'Total War Shogun 2',
         ];
     }
 }
