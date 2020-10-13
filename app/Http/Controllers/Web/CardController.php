ec14bf7f2a28d9bbb517b12226514c9c61884e1f
Mathieu <degracia.mathieu@gmail.com>
2020-10-13 09:31:58 +0200

    On injecte les meilleurs clips dans le show card

--- app/Http/Controllers/Web/CardController.php
+++ app/Http/Controllers/Web/CardController.php
@@ -5,16 +5,20 @@ namespace App\Http\Controllers\Web;
 use View;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-
 use App\Repositories\CardRepository;
+use App\Repositories\ClipRepository;
+use App\Repositories\Criterias\Limit;
+use App\Repositories\Criterias\OrderBy;
 
 class CardController extends Controller
 {
     protected $cardRepository;
+    protected $clipRepository;
 
-    public function __construct(CardRepository $cardRepository)
+    public function __construct(CardRepository $cardRepository, ClipRepository $clipRepository)
     {
         $this->cardRepository = $cardRepository;
+        $this->clipRepository = $clipRepository;
     }
 
     public function index(Request $request)
@@ -26,8 +30,17 @@ class CardController extends Controller
     {
         $card = $this->cardRepository->find($request->id);
 
+        $this->clipRepository->pushCriteria(new Limit(5));
+        $this->clipRepository->pushCriteria(new OrderBy('views', 'DESC'));
+        $this->clipRepository->where([
+            'card_id' => $card->id,
+        ]);
+
+        $clips = $this->clipRepository->all();
+
         return View::make('cards.show', [
             'card' => $card,
+            'clips' => $clips,
         ]);
     }
 }
