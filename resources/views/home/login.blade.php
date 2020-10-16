eae4da9ce3f26a1e78ec9d3a7668186da8a68106
Mathieu <degracia.mathieu@gmail.com>
2020-10-16 21:36:28 +0200

    Ajout d'une page login et du flash message

---
+++ resources/views/home/login.blade.php
@@ -0,0 +1,7 @@
+@extends('app')
+@section('content')
+<section class="section">
+    <h1 class="text-3xl">login</h1>
+    <a href='{{$authorization_url}}'>ceci est un bouton</a>
+</section>
+@endsection
