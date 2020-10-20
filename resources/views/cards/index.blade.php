4e22a84cfbec165e0be496e4697f6553e6faef98
marionj <marionj@focus-home.com>
2020-10-20 10:56:13 +0200

    breadcrumbs

--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -2,13 +2,14 @@
 @section('content')
 <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
     @include('layouts.breadcrumbs', [
-    'breadcrumbs' => ['Home',
+    'breadcrumbs' => [
+    ['Home', route('home')],
     'Fiches']
     ])
 </div>
 <section class="-mt-6 section">
     <h1 class="text-3xl font-semibold leading-8 tracking-tight text-white md:text-4xl sm:text-3xl sm:leading-9">Toutes les fiches</h1>
-    <grid grid-class="grid grid-cols-2 mt-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-x-6 gap-y-10" fetch-url="{{route('api.cards.search')}}" type="cards" :filters="true"/>
+    <grid grid-class="grid grid-cols-2 mt-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-x-6 gap-y-10" fetch-url="{{route('api.cards.search')}}" type="cards" :filters="true"/>
 </section>
 
 @endsection
--- resources/views/layouts/breadcrumbs.blade.php
+++ resources/views/layouts/breadcrumbs.blade.php
@@ -4,7 +4,7 @@
     <div class="font-semibold" key={index}>{{$bread}}</div>
     @else
     <div class="flex items-center">
-        <a href="" class="font-light hover:text-indigo-400">{{$bread}}</a>
+        <a href="{{$bread[1]}}" class="font-light hover:text-indigo-400">{{$bread[0]}}</a>
         <svg class="w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
         </svg>
