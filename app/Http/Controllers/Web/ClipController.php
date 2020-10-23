c2feed3cd81eca37a64985254b6e37edfa99ddfe
Mathieu <degracia.mathieu@gmail.com>
2020-10-23 13:59:25 +0200

    Ajout d'une variable isFavorite

--- app/Http/Controllers/Web/ClipController.php
+++ app/Http/Controllers/Web/ClipController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Web;
 
 use View;
+use App\Models\Clip;
 use Illuminate\Http\Request;
 use App\Repositories\Presenters;
 use Illuminate\Routing\Controller;
@@ -45,10 +46,24 @@ class ClipController extends Controller
 
         $commentCount = $clip->comments->where('active', true)->count();
 
+        $isFavorite = $this->isFavorite($request, $clip);
+
         return View::make('clips.show', [
             'clip' => $clip,
             'comment_count' => $commentCount,
             'clips' => $clips,
+            'isFavorite' => $isFavorite,
         ]);
+    }
+
+    protected function isFavorite(Request $request, Clip $clip): bool
+    {
+        $user = $request->user();
+
+        if (! $user) {
+            return false;
+        }
+
+        return $user->favorites->contains($clip->id);
     }
 }
