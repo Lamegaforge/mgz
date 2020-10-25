42c7d46f49594623f415d7b4c61b9b62b5869fe9
Mathieu <degracia.mathieu@gmail.com>
2020-10-25 14:11:58 +0100

    Ajout des achievements clips

---
+++ tests/Unit/Achievements/ActiveClipsTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Unit\Achievements;
+
+use Tests\TestCase;
+use App\Models\Clip;
+use App\Models\User;
+use App\Services\Achievements\TenActiveClips;
+use App\Services\Achievements\FiveActiveClips;
+use App\Services\Achievements\TwentyActiveClips;
+use App\Services\Achievements\FifteenActiveClips;
+use Illuminate\Foundation\Testing\DatabaseMigrations; 
+
+class ActiveClipsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     * @dataProvider dataProvider
+     */
+    public function eligible(int $times, string $fqcn, bool $expected)
+    {
+        $user = User::factory()->create();
+
+        Clip::factory()
+            ->times($times)
+            ->create([
+                'state' => 'active',
+                'user_id' => $user->id,
+            ]);
+
+        $achievement = new $fqcn($user);
+
+        $this->assertEquals($expected, $achievement->eligible());
+    }
+
+    public function dataProvider(): array
+    {
+        return [
+            [4, $fqcn = FiveActiveClips::class, $expected = false],
+            [5, $fqcn = FiveActiveClips::class, $expected = true],
+            [6, $fqcn = FiveActiveClips::class, $expected = true],
+
+            [9, $fqcn = TenActiveClips::class, $expected = false],
+            [10, $fqcn = TenActiveClips::class, $expected = true],
+            [11, $fqcn = TenActiveClips::class, $expected = true],
+
+            [14, $fqcn = FifteenActiveClips::class, $expected = false],
+            [15, $fqcn = FifteenActiveClips::class, $expected = true],
+            [16, $fqcn = FifteenActiveClips::class, $expected = true],
+
+            [19, $fqcn = TwentyActiveClips::class, $expected = false],
+            [20, $fqcn = TwentyActiveClips::class, $expected = true],
+            [21, $fqcn = TwentyActiveClips::class, $expected = true],
+        ];
+    }
+}
