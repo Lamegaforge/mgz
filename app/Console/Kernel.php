158015429caf67518d49ea24d60ebb4d2a8a51e5
Mathieu <degracia.mathieu@gmail.com>
2020-10-26 13:01:41 +0100

    Marion casse le site

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('clips:aggregate')->dailyAt('10:00');
         $schedule->command('clips:aggregate')->dailyAt('14:00');
 
-        $schedule->command('clips:views-update')->daily();
+        $schedule->command('clips:update')->daily();
 
         $schedule->command('cards:aggregate')->daily();
     }
