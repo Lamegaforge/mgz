0ff2bb3f7bf24d42d3b43b5636bc6780a683d11b
Mathieu <degracia.mathieu@gmail.com>
2020-11-04 09:08:43 +0100

    improve meta

--- app/View/Components/Meta.php
+++ app/View/Components/Meta.php
@@ -2,18 +2,21 @@
 
 namespace App\View\Components;
 
+use App\Models\Clip;
 use Illuminate\View\Component;
 
 class Meta extends Component
 {
+    public $clip;
+
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Clip $clip)
     {
-        //
+        $this->clip = $clip;
     }
 
     /**
--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -1,5 +1,5 @@
-<meta property='og:title' content='Twitch'>
-<meta property='og:description' content='Twitch is the world&#39;s leading video platform and community for gamers.'>
-<meta property='og:image' content='https://static-cdn.jtvnw.net/ttv-static-metadata/twitch_logo3.jpg'>
+<meta property='og:title' content='{{$clip->title}}'>
+<meta property='og:description' content='{{$clip->slug}}'>
+<meta property='og:image' content='{{$clip->thumbnail}}'>
 <meta property='og:url' content='https://www.twitch.tv/lamegaforgelive/clip/FastFreezingOysterFailFish?filter=clips&amp;range=7d&amp;sort=time'>
 <meta property='og:type' content='website'>
