cdbdf2d892dd9825328206fe7d8acc977eba1188
Mathieu <degracia.mathieu@gmail.com>
2020-11-04 09:38:46 +0100

    improve meta

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -1,13 +1,10 @@
 <meta property='og:title' content='{{$clip->title}}'>
 <meta property='og:description' content='{{$clip->slug}}'>
 
-<!-- <meta property="og:url" content="{{route('clips.show', $clip)}}"> -->
-<meta property='og:url' content='https://clips.twitch.tv/PeacefulEnergeticPuffinNononoCat'>
-
+<meta property="og:url" content="{{route('clips.show', $clip)}}">
 <meta property="og:image" content="{{$clip->thumbnail}}">
 <meta property="og:video:type" content="text/html">
-<meta property="og:video:url" content="https://clips.twitch.tv/PeacefulEnergeticPuffinNononoCat">
+<meta property="og:video:url" content="{{$embedUrl}}">
 <meta property="og:video:height" content="720">
 <meta property="og:video:width" content="1280">
 <meta property="og:type" content="video.other">
-
