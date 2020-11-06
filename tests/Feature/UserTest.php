b13b641e83a9ac514ed9dab34545fefafa340cf1
Mathieu <degracia.mathieu@gmail.com>
2020-11-06 09:35:09 +0100

    Add http usertests

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Notification;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class UserTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     */
+    public function get_notifications()
+    {
+        $user = User::factory()
+            ->has(Notification::factory()->unread()->count(2))
+            ->has(Notification::factory()->readed()->count(3))
+            ->create();
+
+        $response = $this->actingAs($user)->get('users/notifications');
+
+        $response->assertStatus(200);
+    }
+}
