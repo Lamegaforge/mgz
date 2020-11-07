a90549db30bcb7d0892ee510e3e97df5b7a2d6ce
Mathieu <degracia.mathieu@gmail.com>
2020-11-07 22:01:53 +0100

    fix bug fav

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -73,7 +73,7 @@
                         </div>
                     </div>
                 </tab>
-                <tab title="Favoris" :value="3">
+                <tab title="Favoris" :value="2">
                     <fetch-list grid-class="grid mt-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-10" fetch-url="{{route('api.favorites.search', $user->id)}}" type="clips" />
                 </tab>
             </tabs>
