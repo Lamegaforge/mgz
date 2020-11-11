3a35bfd2c02f4eb5b840bca77894c26a5b75adb0
Mathieu <degracia.mathieu@gmail.com>
2020-11-11 21:52:23 +0100

    update meta

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -3,8 +3,8 @@
 
 <meta property="og:url" content="{{route('clips.show', $clip)}}">
 <meta property="og:image" content="{{$clip->thumbnail}}">
-<meta property="og:video:type" content="text/html">
+<!-- <meta property="og:video:type" content="text/html"> -->
 <!-- <meta property="og:video:url" content="{{$embedUrl}}"> -->
-<meta property="og:video:height" content="720">
-<meta property="og:video:width" content="1280">
-<meta property="og:type" content="video.other">
+<!-- <meta property="og:video:height" content="720"> -->
+<!-- <meta property="og:video:width" content="1280"> -->
+<!-- <meta property="og:type" content="video.other"> -->
