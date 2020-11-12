31f0a21dd3b506606d1d2dc23c92b275dbda40d0
Mathieu <degracia.mathieu@gmail.com>
2020-11-12 13:45:59 +0100

    Remove test

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('test', function () {
-	throw new Exception("Error Processing Request", 1);
-});
-
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('logout', 'HomeController@logout')->name('logout');
 
