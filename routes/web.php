2f925120bdf04bd4167f364c6f468fc2f5eae673
Mathieu <degracia.mathieu@gmail.com>
2020-11-12 13:24:29 +0100

    test monitoring

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('test', function () {
+	throw new Exception("Error Processing Request", 1);
+});
+
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('logout', 'HomeController@logout')->name('logout');
 
